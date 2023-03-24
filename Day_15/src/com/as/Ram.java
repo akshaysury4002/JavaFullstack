f3bfd29af19df7d941e4e8e2b3bb8cd3b9823831
akshaysury4002 <aasuryawanshi99@gmail.com>
2023-03-24 13:30:07 +0530

    bfcb

---
+++ Day_15/src/com/as/Ram.java
@@ -0,0 +1,13 @@
+package com.as;
+
+public class Ram {
+	
+	 public static void main(String[] args) {
+	        String str = "Hello, World!";
+	        int len = str.length();
+	        
+	       System.out.println(str.indexOf('H'));
+	    }
+	 
+
+}
