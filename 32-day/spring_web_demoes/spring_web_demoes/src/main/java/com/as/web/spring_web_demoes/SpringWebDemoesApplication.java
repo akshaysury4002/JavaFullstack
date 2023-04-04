5802b6daa37b027559327bb72cbcdfc7053d772a
akshaysury4002 <aasuryawanshi99@gmail.com>
2023-04-04 15:27:51 +0530

    spring

---
+++ 32-day/spring_web_demoes/spring_web_demoes/src/main/java/com/as/web/spring_web_demoes/SpringWebDemoesApplication.java
@@ -0,0 +1,13 @@
+package com.as.web.spring_web_demoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebDemoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebDemoesApplication.class, args);
+	}
+
+}
---
+++ 32-day/spring_web_demoes/spring_web_demoes/src/test/java/com/as/web/spring_web_demoes/SpringWebDemoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.as.web.spring_web_demoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebDemoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
