81c8488960964820ca411db6134206b7e1dc0148
akshaysury4002 <aasuryawanshi99@gmail.com>
2023-04-11 10:19:05 +0530

    done

---
+++ 35-day/netti_demos/netti_demos/src/main/java/com/as/netty/NettiDemosApplication.java
@@ -0,0 +1,13 @@
+package com.as.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettiDemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettiDemosApplication.class, args);
+	}
+
+}
---
+++ 35-day/netti_demos/netti_demos/src/test/java/com/as/netty/NettiDemosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.as.netty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettiDemosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
