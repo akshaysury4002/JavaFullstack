3f15a7cef31bfdf56b7996f0ac51366117077990
akshaysury4002 <aasuryawanshi99@gmail.com>
2023-04-11 10:19:45 +0530

    pom xml

---
+++ 37-day/soap_service_demo/soap_service_demo/src/test/java/com/as/soap/SoapServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.as.soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapServiceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
