7112dd4b32193a39ed47289cf572f5ba6fbdb8dd
akshaysury4002 <aasuryawanshi99@gmail.com>
2023-04-26 15:14:54 +0530

    json

--- 49-day/expressjs/index.js
+++ 49-day/expressjs/index.js
@@ -3,18 +3,33 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const customMiddleware = (req, res, next) => {
+    // const id = req.body.id
+    // const name = req.body.name
+    // const category = req.body.category
+    // const price = req.body.price
+
+    const { id, name, category, price } = req.body
+
+    if(name.length < 3) {
+        res.json({ sts :'fail', msg:'invalid name' })
+    }
+    
+    next()
+}
+
+app.use(express.json())
+
 const onGet = (req, res) => {
     res.json({ sts: 'success', msg :'GET Request' })
 }
 function onGet2(req, res) {
     res.json({ sts: 'success', msg :'GET Request' })
 }
+app.get('/', onGet2)
 
-app.get('/', (req, res) => {
-    res.json({ sts: 'success', msg :'GET Request' })
-})
-
-app.post('/', (req, res) => {
+app.post('/', express.json(), customMiddleware , (req, res) => {
+    console.log(req.body)
     res.json({ sts: 'success', msg :'POST Request' })
 })
 
